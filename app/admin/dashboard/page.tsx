395611d806cb2513a6a40ca85f4674aca4a5fa84
Tanagrit Rojgitjanurak <tanagritrojgitjanurak@MacBook-Air-khxngTanagrit.local>
2026-06-14 18:08:13 +0700

    fix

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -33,7 +33,7 @@ export default function StaffApprovalPage() {
     if (!confirmed) return;
 
     // 2. เรียกใช้ Action พร้อมส่ง username ของ Manager
-    const res = await approveStaff(id, managerUsername);
+    const res = await approveStaff({ id, managerUsername });
     
     if (res.success) {
       alert("อนุมัติเรียบร้อยแล้ว");
